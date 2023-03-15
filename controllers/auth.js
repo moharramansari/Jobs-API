e1c47a89ed1373ba6957109aaef8ed3d42a070da
Moharram Ansari <moharramansari@gmail.com>
2023-03-15 17:04:48 +0530

    Added hashedPassword functionalities and validate name, email, password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,15 @@
-const register = async (req, res) => {
-  res.send("register user");
-};
+const User = require("../model/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
 
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({ user });
+};
 const login = async (req, res) => {
-  res.send("login user");
+  res.jso("login user");
 };
 
 module.exports = {
--- model/User.js
+++ model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,8 +21,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
-    maxlength: 12,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
