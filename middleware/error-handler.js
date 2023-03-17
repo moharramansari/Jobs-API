8046071eefa6be679304be7ec63637663eaf9e97
Moharram Ansari <moharramansari@gmail.com>
2023-03-17 12:16:49 +0530

    Added mongoose duplicate error handler functionalities.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,10 +2,22 @@ const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customerError = {
+    //set default
+    statusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong please try again later",
+  };
+
   if (err instanceof CustomAPIError) {
-    return res.status(err.StatusCodes).json({ msg: err.message });
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  if (err.code && err.code === 11000) {
+    customerError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, Please choose another value`
+    customerError.statusCode = 400
+  }
+
+  return res.status(customerError.statusCode).json({ msg: customerError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
