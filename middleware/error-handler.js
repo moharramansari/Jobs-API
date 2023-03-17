ad957a11816f1ddaabe77013888d0db91312339a
Moharram Ansari <moharramansari@gmail.com>
2023-03-17 12:53:36 +0530

    Added some customError functionalities in the errorHandler file and fixed some changes in auth controller

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -8,15 +8,25 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong please try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === "ValidationError") {
+    console.log(Object.values(err.errors));
+    customerError.msg = Object.values(err.errors)
+      .map((itme) => itme.message)
+      .join(",");
+    customerError.statusCode = 400;
   }
 
   if (err.code && err.code === 11000) {
-    customerError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, Please choose another value`
-    customerError.statusCode = 400
+    customerError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, Please choose another value`;
+    customerError.statusCode = 400;
   }
-
+  //  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customerError.statusCode).json({ msg: customerError.msg });
 };
 
